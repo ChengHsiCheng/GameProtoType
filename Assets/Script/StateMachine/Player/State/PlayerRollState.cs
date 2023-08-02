b50948088b98eb2762efa524a0be723fdfdfe7d9
ChengHsiCheng <96904538+ChengHsiCheng@users.noreply.github.com>
2023-08-03 05:41:13 +0800

    更改Boss01場景、玩家攻擊動畫、玩家翻滾機制
    玩家翻滾會面向輸入方向

--- Assets/Script/StateMachine/Player/State/PlayerRollState.cs
+++ Assets/Script/StateMachine/Player/State/PlayerRollState.cs
@@ -7,6 +7,8 @@ public class PlayerRollState : PlayerBaseState
     private readonly int RollHash = Animator.StringToHash("Roll");
     private const float CrossFadeDuration = 0.1f;
 
+    Vector3 faceDis;
+
     public PlayerRollState(PlayerStateMachine stateMachine) : base(stateMachine)
     {
     }
@@ -20,13 +22,18 @@ public class PlayerRollState : PlayerBaseState
         stateMachine.Animator.CrossFadeInFixedTime(RollHash, CrossFadeDuration);
 
         stateMachine.Info.SetInvulnerable(true);
+
+        faceDis = CalculateMovement();
     }
 
     public override void Tick(float deltaTime)
     {
         float normalizedTime = GetNormalizedTime(stateMachine.Animator, "Roll");
 
-        // 判斷是否可以操作
+        if (normalizedTime <= 0.2)
+        {
+            FaceMovementDirection(faceDis, deltaTime);
+        }
 
         if (normalizedTime <= 0.4)
         {
@@ -46,5 +53,36 @@ public class PlayerRollState : PlayerBaseState
         stateMachine.Info.SetInvulnerable(false);
 
         stateMachine.SetCanCancel(true);
+    }
+
+    /// <summary>
+    /// 計算玩家移動向量
+    /// </summary>
+    private Vector3 CalculateMovement()
+    {
+        Vector3 forward = stateMachine.MainCameraTransform.forward;
+        Vector3 right = stateMachine.MainCameraTransform.right;
+
+        forward.y = 0;
+        right.y = 0;
+
+        forward.Normalize();
+        right.Normalize();
+
+        // 根據玩家輸入的移動值和相機的前方與右方向量計算最終的移動向量
+        return forward * stateMachine.InputReader.MovementValue.y +
+            right * stateMachine.InputReader.MovementValue.x;
+    }
+
+    /// <summary>
+    /// 面向移動方向
+    /// </summary>
+    private void FaceMovementDirection(Vector3 movemnt, float deltaTime)
+    {
+        // 使用插值的方式將角色的旋轉逐漸調整為面向移動方向
+        stateMachine.transform.rotation = Quaternion.Lerp(
+            stateMachine.transform.rotation,
+            Quaternion.LookRotation(movemnt),
+            deltaTime * stateMachine.RotationDamping * 2);
     }
 }
