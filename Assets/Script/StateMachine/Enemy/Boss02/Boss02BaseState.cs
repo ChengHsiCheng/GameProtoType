5dc25902b9bfadbfa739d1637e192eb626ba8d69
ChengHsiCheng <96904538+ChengHsiCheng@users.noreply.github.com>
2023-10-01 22:24:44 +0800

    整理資料夾

---
+++ Assets/Script/StateMachine/Enemy/Boss02/Boss02BaseState.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Boss02BaseState : State
+{
+    protected Boss02StateMachine stateMachine;
+
+    public Boss02BaseState(Boss02StateMachine stateMachine)
+    {
+        this.stateMachine = stateMachine;
+    }
+
+}
---
+++ Assets/Script/StateMachine/Enemy/Boss02/Boss02StateMachine.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss02StateMachine : StateMachine, Enemy
+{
+    [field: SerializeField] public Animator Animator { get; private set; }
+    [field: SerializeField] public CharacterController Controller { get; private set; }
+    [field: SerializeField] public SkinnedMeshRenderer Material { get; private set; }
+
+    private void Start()
+    {
+        GameManager.enemys.Add(this);
+
+        SwitchState(new Boss02StartState(this));
+
+    }
+
+    public override void OnGameTogglePause(bool isPause)
+    {
+        if (!isPause && freezeTime != 0)
+        {
+            return;
+        }
+
+
+        int intValue = isPause ? 0 : 1; // 把canMove轉成1或0
+        Animator.SetFloat("AnimationSpeed", intValue);
+    }
+
+    public override void SetCanMove(bool value)
+    {
+        SetCanMove(value, 0);
+
+        Material.material.SetFloat("_Petrifaction", 1);
+    }
+
+    public override void SetCanMove(bool value, float time)
+    {
+        this.canMove = canMove;
+        this.freezeTime = Time.time + freezeTime;
+
+        int intValue = canMove ? 1 : 0; // 把canMove轉成1或0
+
+        Animator.SetFloat("AnimationSpeed", intValue);
+    }
+
+    public void BePetrify()
+    {
+        SetCanMove(false, 3f);
+
+        Material.material.SetFloat("_Petrifaction", 0);
+    }
+}
